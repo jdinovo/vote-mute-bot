9820b00abc0bd5530f2ddcfaf8d49e011c02a921
jdinovo <43574189+jdinovo@users.noreply.github.com>
2021-07-19 16:52:16 -0400

    Enhancements to active user tracking - Votes required now scales with number of active users

--- commands/votemute.js
+++ commands/votemute.js
@@ -4,73 +4,114 @@ module.exports = {
     name: 'votemute',
     description: 'Call a vote to mute a given user.',
     aliases: ['votemute', 'vm'],
-    cooldown: 20,
+    cooldown: 30,
     args: true,
     usage: '<userToMute>',
     async execute(message, args) {
 
+        const sTime = 20;
+        const minVotesRequired = Math.ceil(activeUsers.size * 0.6);
+
         let userId = args[0].replace(/\D/g, '');
-        let userToMute = message.mentions.members.first();
+        let userToMute = await message.guild.members.fetch(userId).catch(console.error);
         let muteRole = message.guild.roles.cache.find(r => r.name === "muted");
 
+        if (userId === '' || !userToMute) {
+            message.channel.send('Invalid user given.');
+            return;
+        }
         if (userToMute.user.bot) {
             message.channel.send('You can\'t mute bots.');
             return;
         }
-
         if (!muteRole) {
             message.channel.send('Please create a `muted` role.');
             return;
         }
-
         if (userToMute.roles.cache.has(muteRole.id)) {
             message.channel.send(new Discord.MessageEmbed().setColor('#FA1111').setTitle('Muted').setDescription(`<@${userId}> is already muted.`));
             return;
         }
 
-
-        const embed = new Discord.MessageEmbed().setColor('#AAAA11').setTitle('Vote to Mute').setDescription(`Should <@${userId}> be muted?`);
+        const embed = new Discord.MessageEmbed().setColor('#DDDD22').setTitle('Vote to Mute').setDescription(`Should <@${userId}> be muted?`);
 
         let rMessage = await message.channel.send(embed);
 
         rMessage.react('👍').then(() => rMessage.react('👎'));
 
         const filter = (reaction, user) => {
-            return ['👍', '👎'].includes(reaction.emoji.name) && user.id !== userToMute.id;
+            return ['👍', '👎'].includes(reaction.emoji.name) && !user.bot;
         };
 
-        const collector = rMessage.createReactionCollector(filter, { time: 15000 });
+        const collector = rMessage.createReactionCollector(filter, { dispose: true, time: 25000 });
 
+        let upvotes = 0;
+        let downvotes = 0;
 
         collector.on('collect', (reaction, user) => {
+            switch(reaction.emoji.name) {
+                case '👍':
+                    upvotes++;
+                    break;
+                case '👎':
+                    downvotes++;
+                    break;
+                default:
+                    break;
+            }
+
             console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
         });
 
+        collector.on('remove', (reaction, user) => {
+            switch(reaction.emoji.name) {
+                case '👍':
+                    if(upvotes > 0)
+                        upvotes--;
+                    break;
+                case '👎':
+                    if (downvotes > 0)
+                        downvotes--;
+                    break;
+                default:
+                    break;
+            }
+
+            console.log(`Removed ${reaction.emoji.name} from ${user.tag}`);
+        });
+
         collector.on('end', collected => {
-            const upvote = collected.get('👍');
-            const downvote = collected.get('👎');
 
-            const upvotes = upvote ? upvote.count - 1 : 0;
-            const downvotes = downvote ? downvote.count - 1 : 0;
+            const minVoteRequirement = ((upvotes + downvotes) >= minVotesRequired);
 
-            const minVoteRequirement = ((upvotes + downvotes) > 3);
+            console.log('Up: ', upvotes);
+            console.log('Down: ', downvotes);
 
             if (upvotes > downvotes && minVoteRequirement) {
                 console.log('VOTE PASSED');
 
-                rMessage.edit(new Discord.MessageEmbed().setColor('#11FA11').setTitle('Vote Passed').setDescription(`<@${userId}> has been muted for 5 minutes!`));
+                const maxTime = 30 * 60
+
+                let muteTime = (upvotes >= activeUsers.size && downvotes < 1) ? maxTime : ((sTime * upvotes) * Math.abs(upvotes - downvotes));
+
+                // cap at 30mins
+                muteTime = (muteTime > maxTime) ? maxTime : muteTime;
+
+                rMessage.edit(new Discord.MessageEmbed().setColor('#11FA11').setTitle('Vote Passed')
+                                .setDescription(`<@${userId}> has been muted for ${muteTime > 60 ? Math.floor(muteTime / 60) + ' minute(s)' : muteTime + ' seconds(s)'}!`)
+                                .addField('Results', `Yes: ${upvotes} No: ${downvotes}`));
 
                 userToMute.roles.add(muteRole).catch(console.error);
 
                 setTimeout(function() {
                     userToMute.roles.remove(muteRole).catch(console.error);
                     console.log('USER UNMUTED');
-                }, (5 * 60000));
+                }, muteTime * 1000);
             } else {
                 console.log('VOTE FAILED');
                 const failEmbed = new Discord.MessageEmbed().setColor('#FA1111').setTitle('Vote Failed').setDescription(`<@${userId}> has not been muted.`);
                 if (!minVoteRequirement) {
-                    failEmbed.addField('Reason', 'Not enough votes.');
+                    failEmbed.addField('Reason', `Not enough votes. (min. ${minVotesRequired})`);
                 } else {
                     failEmbed.addField('Reason', `Yes: ${upvotes} No: ${downvotes}`);
                 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Discord = require("discord.js");
+const unmute = require("./utils/unmute.js");
 global.bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 const { prefix, token } = require('./config.json');
 bot.commands = new Discord.Collection();
@@ -7,80 +8,100 @@ const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('
 
 // get commands
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    bot.commands.set(command.name, command);
 }
 
 const cooldowns = new Discord.Collection();
-const urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
-const urlTestRegex = /(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+/;
 
 console.log('~~~~~~~~Running Vote Mute Bot~~~~~~~~');
 let botID;
+global.activeUsers = new Map();
 
 bot.on("ready", () => {
     botID = bot.user.id;
     console.log('botID: ' + botID);
-	bot.user.setStatus('online');
-	bot.user.setActivity('', { type: 'WATCHING' });
+    bot.user.setStatus('online');
+    bot.user.setActivity('', { type: 'WATCHING' });
+
+    unmute.all();
+    
 });
 
 bot.on('message', message => {
 
-	// prevent responding to self
-	if (message.author.bot) return;
+    // prevent responding to self
+    if (message.author.bot) return;
 
-	// ignore anything that does not start with prefix
-	if (!message.content.startsWith(prefix)) return;
+    // clear user if exists
+    activeUsers.delete(message.author.id);
+    // add user to list
+    activeUsers.set(message.author.id, Date.now());
 
-	// get arguements
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
+    let minsAgo = Date.now() - (5 * 60000);
 
-	// find command 
-	const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    for (let [key, value] of activeUsers) {
+        if (value < minsAgo) {
+            activeUsers.delete(key);
+            console.log('Removing inactive user: ', key);
+            continue;
+        }
+        break;
+    }
 
-	if (!command) return;
+    console.log(activeUsers);
 
-	// if the command requires arguements and none are provided display message
-	if (command.args && !args.length) {
-		let reply = `This command requires arguments, ${message.author}!`;
+    // ignore anything that does not start with prefix
+    if (!message.content.startsWith(prefix)) return;
 
-		if (command.usage) {
-			reply += `\nProper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
+    // get arguements
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-		return message.channel.send(reply);
-	}
+    // find command 
+    const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-	// ---- set up for cooldowns below ----
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
+    if (!command) return;
 
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
+    // if the command requires arguements and none are provided display message
+    if (command.args && !args.length) {
+        let reply = `This command requires arguments, ${message.author}!`;
 
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+        if (command.usage) {
+            reply += `\nProper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
 
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`you cannot use \`${command.name}\` for ${timeLeft.toFixed(1)} more second(s).`);
-		}
-	}
+        return message.channel.send(reply);
+    }
 
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    // ---- set up for cooldowns below ----
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
 
-	// execute commands
-	try {
-		command.execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('There was an error trying to execute that command!');
-	}
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`you cannot use \`${command.name}\` for ${timeLeft.toFixed(1)} more second(s).`);
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    // execute commands
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('There was an error trying to execute that command!');
+    }
 });
 
 bot.login(token);
